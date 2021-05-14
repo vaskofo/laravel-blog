9d807df80ae266e6f16554ac149e233f228f1143
Sam <sam@binshops.com>
2021-05-13 19:12:29 -0500

    changes edit post page in admin panel

--- src/Views/binshopsblog_admin/posts/edit_post.blade.php
+++ src/Views/binshopsblog_admin/posts/edit_post.blade.php
@@ -2,9 +2,16 @@
 @section("content")
 
 
-    <h5>Admin - Editing post
-    <a target='_blank' href='{{$post->url()}}' class='float-right btn btn-primary'>View post</a>
-    </h5>
+    @if(!$post->is_tutorial)
+        <h5>Editing post
+            <a target='_blank' href='{{$post->url()}}' class='float-right btn btn-primary'>View post</a>
+        </h5>
+    @else
+        <h5>Editing Tutorial Post
+            <a target='_blank' href='{{$post->url()}}' class='float-right btn btn-primary'>View Tutorial Post</a>
+        </h5>
+    @endif
+
 
     <form method='post' action='{{route("binshopsblog.admin.update_post",$post->id)}}'  enctype="multipart/form-data" >
 
